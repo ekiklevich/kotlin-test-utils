e05b92c11ea66d599c58a9fd847a238896cb6038
EvgeniyKiklevich <Evgeniy.Kiklevich_ext@leroymerlin.ru>
2025-08-13 13:13:41 +0300

    Initial commit

---
+++ src/test/kotlin/com/ek/demo/TestExample.kt
@@ -0,0 +1,13 @@
+package com.ek.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestExample {
+
+	@Test
+	fun contextLoads() {
+		println("example")
+	}
+}
